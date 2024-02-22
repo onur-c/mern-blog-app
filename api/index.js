5542e6cbba2e530d8be0878aad2003607821f44f
Onur Celik <60694762+onur-c@users.noreply.github.com>
2024-02-22 14:10:48 +0300

    fix minor problem:gitignore

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserModel from "./models/User.js";
+import "dotenv/config.js";
+
 const PORT = 4000;
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
-  const { userName, password } = req.body;
-  res.json({
-    requestData: { userName, password },
-  });
+mongoose.connect(process.env.MONGO_SECRET);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await UserModel.create({ username, password });
+
+  res.json(user);
 });
 
 app.listen(PORT);
